b44f68531f2e623196678efa48fad5f160c65f20
Darya Semina <darina1970@mail.ru>
2025-08-17 21:53:38 +0700

    feat: add settings page

--- home.js
+++ home.js
@@ -8,10 +8,23 @@ document.getElementById(
   "welcomeText"
 ).textContent = `WELCOME TO THE NOT FIGHT CLUB, ${playerName}!`;
 
-document.getElementById("startFight").addEventListener("click", () => {
+const music = document.getElementById("bgMusic");
+music.volume = 0.3;
+music.play().catch(() => {
+  console.log("Autoplay blocked. User interaction required.");
+});
+document.getElementById("fightBtn").addEventListener("click", () => {
   alert("В разработке...");
+  localStorage.setItem("playerCharacter", Math.floor(Math.random() * 3) + 1);
+  localStorage.setItem("enemyCharacter", Math.floor(Math.random() * 2) + 1);
+  window.location.href = "fight.html";
 });
 
-document.getElementById("characterPage").addEventListener("click", () => {
+document.getElementById("chooseBtn").addEventListener("click", () => {
   alert("В разработке...");
+  window.location.href = "character.html";
+});
+
+document.getElementById("settingsBtn").addEventListener("click", () => {
+  window.location.href = "settings.html";
 });
---
+++ settings.js
@@ -0,0 +1,5 @@
+const playerNameInput = document.getElementById("playerName");
+const saveNameBtn = document.getElementById("saveName");
+const volumeControl = document.getElementById("volumeControl");
+const bgMusic = document.getElementById("bgMusic");
+const backBtn = document.getElementById("backBtn");
